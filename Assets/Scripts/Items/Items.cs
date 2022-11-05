42a48c57038284474b94c235a0422f2724c28157
Serhii <serikgorbanev@gmail.com>
2022-11-05 01:03:23 +0100

    Item and Inventory rework... again
    ye, i make Item third time and we don't need scriptable objects anymore

--- Assets/Scripts/Items/Items.cs
+++ Assets/Scripts/Items/Items.cs
@@ -1,44 +1,113 @@
-/*s*/using System.Collections;
-/*s*/using System.Collections.Generic;
-/*s*/using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-namespace Items
+public enum ItemsTypes
 {
-    public enum ItemTypes
+    Nothing,
+    IronOre,
+    CopperOre,
+    Coal,
+}
+
+
+public class Item
+{
+    static Dictionary<ItemsTypes, int> itemTypesMaxStacks = new Dictionary<ItemsTypes, int>()
     {
-        Nothing = 0,
-        IronOre = 100,
-        CopperOre = 100
+        { ItemsTypes.Nothing, 0 },
+        { ItemsTypes.IronOre, 100 },
+        { ItemsTypes.CopperOre, 100 },
+    };
+
+    public int count = 0;
+    public new ItemsTypes itemType = ItemsTypes.Nothing;
+
+    public Item(int count = 0, ItemsTypes itemType = ItemsTypes.Nothing)
+    {
+        this.count = count;
+        this.itemType = itemType;
+    }
+    
+    public Item putItem(Item item)//пояснення чому не void: я хочу щоб на мишці висів 1 item типу як предмет який тримає мишка, і при визові цієї функції вміст мишки буде замінюватись на Item який повертає ця функція
+    {
+        if (itemType != item.itemType)
+        {
+            Item TakenItem = this;
+
+            count = item.count;
+            itemType = item.itemType;
+
+            return TakenItem;
+        }
+        else
+        {
+            if (item.count + count <= itemTypesMaxStacks[item.itemType])
+            {
+                count += item.count;
+                return new Item();
+            }
+            else
+            {
+                item.count -= itemTypesMaxStacks[item.itemType] - count;
+                count = itemTypesMaxStacks[item.itemType];
+                return item;
+            }
+        }
+        return new Item();
     }
 
-    public class Item
+    public static Item operator +(Item firstItem, Item secondItem) => new Item(firstItem.count + secondItem.count, firstItem.itemType);
+
+    public static bool operator >(Item firstItem, Item secondItem) => firstItem.count > secondItem.count;
+
+    public static bool operator <(Item firstItem, Item secondItem) => firstItem.count < secondItem.count;
+
+    public static bool operator >(Item[] itemArray, Item item)
     {
-        public int count;
-        public ItemTypes itemType = ItemTypes.Nothing;
-        public const int maxStack = (int)itemType;
-        public Item()
+        foreach (Item _item in itemArray)
         {
-            count = 0;
+            if (_item.itemType == item.itemType)
+            {
+                return item.count > _item.count;
+            }
         }
+        return false;
     }
 
-    public class ItemIronOre : Item
+    public static bool operator <(Item[] itemArray, Item item)
     {
-        public const ItemTypes itemType = ItemTypes.IronOre;
-        public const int maxStack = (int)itemType;
-        public ItemIronOre(int count)
+        foreach (Item _item in itemArray)
         {
-            this.count = count;
+            if (_item.itemType == item.itemType)
+            {
+                return item.count < _item.count;
+            }
         }
+        return false;
     }
 
-    public class ItemCopperOre : Item
+    public static bool operator >(List<Item> itemArray, Item item)
     {
-        public const ItemTypes itemType = ItemTypes.CopperOre;
-        public const int maxStack = (int)itemType;
-        public ItemCopperOre(int count)
+        foreach (Item _item in itemArray)
         {
-            this.count = count;
+            if (_item.itemType == item.itemType)
+            {
+                return item.count > _item.count;
+            }
         }
+        return false;
+    }
+
+    public static bool operator <(List<Item> itemArray, Item item)
+    {
+        foreach (Item _item in itemArray)
+        {
+            if (_item.itemType == item.itemType)
+            {
+                return item.count < _item.count;
+            }
+        }
+        return false;
     }
 }
--- Assets/Scripts/Player/Inventory.cs
+++ Assets/Scripts/Player/Inventory.cs
@@ -1,7 +1,6 @@
-﻿/*s*/using System.Collections;
-/*s*/using System.Collections.Generic;
-/*s*/using UnityEngine;
-/*s*/using Items;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
 public class Inventory : MonoBehaviour
 {
@@ -18,38 +17,8 @@ public class Inventory : MonoBehaviour
         }
     }
 
-    public Item PutItem(int slotIndex, Item item)//пояснення чому не void: я хочу щоб на мишці висів 1 item типу як предмет який тримає мишка, і при визові цієї функції вміст мишки буде замінюватись на Item який повертає ця функція
-    {
-        if (item.itemType != items[slotIndex].itemType)
-        {
-            Item TakenItem = items[slotIndex];
-            items[slotIndex] = item;
-            return TakenItem;
-        }
-        else
-        {
-            if (item.count + items[slotIndex].count <= (int)item.itemType)
-            {
-                items[slotIndex].count += item.count;
-                return new Item();
-            }
-            else
-            {
-                item.count -= (int)item.itemType - items[slotIndex].count;
-                items[slotIndex].count = (int)item.itemType;
-                return item;
-            }
-        }
-        return new Item();
-    }
-
     public void Sort()
     {
         //дороблюй сергійко, дороблюй
-    }
-
-    void Update()
-    {
-        Debug.Log(new ItemIronOre(1).GetType());
     }
 }
