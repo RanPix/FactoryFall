06c3c2cf5c5d55f2a737aebca4e6d59e082fe756
RanPix <darkorden16@gmail.com>
2022-12-13 20:35:54 +0200

    fixed error

--- Assets/Scripts/Mobs/TargetZoneTriggerScript.cs
+++ Assets/Scripts/Mobs/TargetZoneTriggerScript.cs
@@ -1,7 +1,5 @@
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
+using Player;
 
 public class TargetZoneTriggerScript : MonoBehaviour
 {
